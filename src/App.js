55bc4d971b43dfbae7d5ff35aeb280b8cdfa3991
mnavoy4 <mnavoy4@gmail.com>
2020-10-21 13:09:29 -0600

    installed react-router-dom and setting up header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Home</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <h1>Michael Navoy</h1>
+          <div className='logo'>
+            <img src={require('./assets/images/headshot.png')} alt=''/>
+          </div>
+          <button className='nav-toggle' aria-label='toggle navigation'>
+            <span className='hamburger'></span>
+          </button>
+          <nav className='nav'>
+            <ul className='nav-list'>
+              <li className='nav-item'>
+                <Link to='/' className='nav-link'>Home</Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/about' className='nav-link'>About Me</Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/work' className='nav-link'>My Work</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className='main'>
+          <div className='content'>
+            
+          </div>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 

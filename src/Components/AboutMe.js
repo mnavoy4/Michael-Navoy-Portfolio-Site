82b4d5d72c75f6f99bb2da03ebe9fb9b65fb1727
mnavoy4 <mnavoy4@gmail.com>
2020-10-22 11:03:25 -0600

    finished styling about me section

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -5,21 +5,25 @@ export default function AboutMe(){
     <section className='about-me' id='about'>
       <h2 className='section-title section-title-about'>Who I am</h2>
       <p className='section-subtitle section-subtitle-about'>
-        Some Text
+        Full stack developer based in Denver
       </p>
       <div className='about-me-body'>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sollicitudin iaculis bibendum.
           Aliquam erat volutpat. Cras commodo tincidunt metus ac venenatis.
-          Suspendisse finibus dapibus molestie. Donec consectetur accumsan scelerisque.
+          Suspendisse finibus dapibus molestie.
         </p>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sollicitudin iaculis bibendum.
           Aliquam erat volutpat. Cras commodo tincidunt metus ac venenatis.
-          Suspendisse finibus dapibus molestie. Donec consectetur accumsan scelerisque.
+          Suspendisse finibus dapibus molestie.
         </p>
       </div>
-      <img src={require('../assets/images/fishing.png')} alt='picture of Michael and a friend'/>
+      <img
+        src={require('../assets/images/fishing-large.png')}
+        alt='picture of Michael and a friend'
+        className='about-me-image'
+      />
     </section>
   )
 }

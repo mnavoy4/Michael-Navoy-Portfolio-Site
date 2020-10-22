a859ee838db354e34fa9b70c71d6ccb6693f3a06
mnavoy4 <mnavoy4@gmail.com>
2020-10-22 12:38:08 -0600

    styling footer and fixing links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer(){
   return(
-    <footer>
+    <footer className='footer'>
       <a href='mailto:mnavoy4@gmail.com' className='footer-link'>mnavoy4@gmail.com</a>
       <ul className='social-list'>
         <li className='social-list-item'>
-          <a href='https://github.com' className='social-list-link'>Link 1</a>
+          <a href='https://github.com/mnavoy4' target='_blank' className='social-list-link'><FontAwesomeIcon icon={faGithub}/></a>
         </li>
         <li className='social-list-item'>
-          <a href='https://github.com' className='social-list-link'>Link 2</a>
+          <a href='https://www.linkedin.com/in/michael-navoy/' target='_blank' className='social-list-link'><FontAwesomeIcon icon={faLinkedin}/></a>
         </li>
         <li className='social-list-item'>
-          <a href='https://github.com' className='social-list-link'>Link 3</a>
+          <a href='https://medium.com/@mnavoy4' target='_blank' className='social-list-link'><FontAwesomeIcon icon={faMedium}/></a>
         </li>
       </ul>
     </footer>
--- src/Components/MyWork.js
+++ src/Components/MyWork.js
@@ -4,13 +4,17 @@ import { Link } from 'react-router-dom';
 export default function MyWork(){
   return (
     <section className='my-work' id='work'>
-      <h2 className='section-title'>My Work</h2>
-      <p className='section-subtitle'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aliquam sollicitudin iaculis bibendum. Aliquam erat volutpat. Cras commodo tincidunt metus ac venenatis.
-        Suspendisse finibus dapibus molestie. Donec consectetur accumsan scelerisque.
+      <h2 className='section-title section-title-work'>My Work</h2>
+      <p className='section-subtitle section-subtitle-work'>
+        A selection of my range of work
       </p>
       <div className='portfolio'>
+        <Link to='https://google.com' className='portfolio-item'>
+          <img src={require('../assets/images/fishing.png')} alt='pic' className='portfolio-image'/>
+        </Link>
+        <Link to='https://google.com' className='portfolio-item'>
+          <img src={require('../assets/images/fishing.png')} alt='pic' className='portfolio-image'/>
+        </Link>
         <Link to='https://google.com' className='portfolio-item'>
           <img src={require('../assets/images/fishing.png')} alt='pic' className='portfolio-image'/>
         </Link>

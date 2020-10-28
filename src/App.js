6e0bd9be48de5874b5b7878e59c0c5c2cc0536a2
mnavoy4 <mnavoy4@gmail.com>
2020-10-27 19:48:57 -0600

    nav opening on click

--- src/App.js
+++ src/App.js
@@ -1,39 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import IntroductionContainer from './Containers/IntroductionContainer';
 import AboutMe from './Components/AboutMe';
 import MyWork from './Components/MyWork';
 import Footer from './Components/Footer';
-// import './App.css';
 
-function App() {
-  return (
+function App(){
+
+  const [navOpen, setNavOpen] = useState(false);
+
+  return(
     <BrowserRouter>
       <div className="App">
         <header>
           <div className='logo'>
-            <img src={require('./assets/images/headshot.png')} alt=''/>
+            <img src={require('./assets/images/logo.png')} alt=''/>
           </div>
-          <button className='nav-toggle' aria-label='toggle navigation'>
+          <button onClick={() => setNavOpen(!navOpen)} className='nav-toggle'>
             <span className='hamburger'></span>
           </button>
-          <nav className='nav'>
-            <ul className='nav-list'>
-              <li className='nav-item'>
-                <Link to='/' className='nav-link'>Home</Link>
-              </li>
-              <li className='nav-item'>
-                <Link to='/about' className='nav-link'>About Me</Link>
-              </li>
-              <li className='nav-item'>
-                <Link to='/services' className='nav-link'>My Services</Link>
-              </li>
-              <li className='nav-item'>
-                <Link to='/work' className='nav-link'>My Work</Link>
-              </li>
-            </ul>
-          </nav>
+          {
+            navOpen ? 
+              <nav className='nav-open nav'>
+                <ul className='nav-list'>
+                  <li className='nav-item'>
+                    <Link to='/' className='nav-link'>Home</Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link to='/about' className='nav-link'>About Me</Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link to='/services' className='nav-link'>My Services</Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link to='/work' className='nav-link'>My Work</Link>
+                  </li>
+                </ul>
+              </nav>
+            :
+              <nav className='nav'>
+                <ul className='nav-list'>
+                  <li className='nav-item'>
+                    <Link to='/' className='nav-link'>Home</Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link to='/about' className='nav-link'>About Me</Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link to='/services' className='nav-link'>My Services</Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link to='/work' className='nav-link'>My Work</Link>
+                  </li>
+                </ul>
+              </nav>
+          }
         </header>
         <main className='main'>
           <div className='content'>
@@ -45,7 +67,7 @@ function App() {
         </main>
       </div>
     </BrowserRouter>
-  );
+  )
 }
 
 export default App;
